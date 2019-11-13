27d2db87cc39da91384c5152d0de22c64989227e
Xu Qi <xuqi@xiaobangtouzi.com>
2019-11-13 20:43:29 +0800

    wait与sleep方法

--- src/main/java/com/shengsiyuan/concurrency1/MyTest1.java
+++ src/main/java/com/shengsiyuan/concurrency1/MyTest1.java
@@ -1,7 +1,15 @@
 package com.shengsiyuan.concurrency1;
 
+/*
+   在调用wait()方法时，线程必须持有锁，在调用wait()方法后，线程就会释放该对象的锁
+   在调用Thread类的sleep方法时，线程是不会释放对象的锁
+ */
 public class MyTest1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws InterruptedException {
+        Object object = new Object();
+        synchronized (object){
+            object.wait();
+        }
     }
 }
