03d6d5b50f741c85c01e518bb96fc138579f4aa5
Xu Qi <xuqi@xiaobangtouzi.com>
2019-11-27 14:06:02 +0800

    synchronized详解

---
+++ src/main/java/com/shengsiyuan/concurrency3/MyTest5.java
@@ -0,0 +1,18 @@
+package com.shengsiyuan.concurrency3;
+
+public class MyTest5 {
+
+    private Object object = new Object();
+
+    public void method(){
+        synchronized (object){
+            System.out.println("hello world");
+        }
+        synchronized (object){
+            System.out.println("welcome");
+        }
+        synchronized (object){
+            System.out.println("person");
+        }
+    }
+}
